b0b514e59dde3da7f77b141394a8a6f0b87f0cc4
VortexAI <s.c.a.beukers@student.tue.nl>
2026-08-16 21:44:36 +0200

    added basic implemenation for settings screen

---
+++ screens/settings_screen.gd
@@ -0,0 +1,38 @@
+extends Node2D
+
+var start_screen_path = "res://screens/start_screen.tscn"
+var config = ConfigFile.new()
+
+@onready var back_sprite: Sprite2D = $"./BackSprite"
+
+func _ready() -> void:
+    var err = config.load("user://config.ini")
+
+    if err != OK:
+        return
+
+    var selected_back_sprite = config.get_value("card", "back_color", 0)
+
+    var back_sprite_select: OptionButton = $"./OptionsContainer/BackColorSelect"
+    back_sprite_select.select(selected_back_sprite)
+    back_sprite.frame = selected_back_sprite
+
+    var selected_turn_style = config.get_value("game", "turn_style", 0);
+
+    var turn_style_select: OptionButton = $"./OptionsContainer/TurnStyleSelect"
+    turn_style_select.select(selected_turn_style)
+
+func _on_color_change(index: int):
+    back_sprite.frame = index
+    config.set_value("card", "back_color", index)
+
+func _on_turn_style_change(index: int):
+    config.set_value("game", "turn_style", index)
+
+func _on_save():
+    print('save')
+    config.save("user://config.ini")
+
+func _on_return():
+    print('return')
+    get_tree().change_scene_to_file(start_screen_path)
--- screens/start_screen.gd
+++ screens/start_screen.gd
@@ -1,10 +1,11 @@
 extends Node2D
 
-var solitaire_scene = load("res://solitaire/solitaire.tscn")
+var solitaire_scene = preload("res://solitaire/solitaire.tscn")
+var settings_scene = preload("res://screens/settings_screen.tscn")
 
 func _on_start_solitaire():
     
     get_tree().change_scene_to_packed(solitaire_scene)
 
 func _on_settings():
-    pass
+    get_tree().change_scene_to_packed(settings_scene)
